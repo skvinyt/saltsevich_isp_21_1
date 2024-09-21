fadd77ec14819b5b7c0c15dc09a3dd27a2014b5b
skvinyt <skvinyt@yandex.ru>
2024-09-22 00:11:25 +0700

    Add file index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="Author" lang="ru" content="Сальцевич Денис">
+        <meta name="description" content="Линейный вычислительный процесс">
+        <title>Лабораторная работа № 3</title>
+        <link rel="stylesheet" href="/index_style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <main>
+            <header>
+                <div class="site-title"><a href="index.php">LaboTronick</a></div>
+                <div class="buttons">
+                    <a href="index_lr_3.php">ЛАБА 3</a>
+                    <a href="">Страница 2</a>
+                    <a href="">Страница 3</a>
+                </div>
+            </header>
+            <div class="background-image"></div>
+        </main>
+        <footer>
+            <div class="footer">
+                <div class="row">
+                    <a href="#"><i class="fa fa-telegram"></i></a>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                    <a href="#"><i class="fa fa-vk"></i></a>
+                    <a href="#"><i class="fa fa-github"></i></a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
